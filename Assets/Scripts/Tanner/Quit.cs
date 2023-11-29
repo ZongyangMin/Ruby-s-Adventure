f9d7c38e9dd6c0088ab2d14107b2e1286453a236
Zongyang Min <mzongyang@gmail.com>
2023-11-29 02:59:26 -0500

    Fifth commit

---
+++ Assets/Scripts/Tanner/Quit.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    //Tanner Hedges 
+    //Quit Game Function
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Tanner/Start.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+    //Tanner Hedges 
+    //Start Game Function
+    public void gameStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
